41533a152c9b32fe43cbc6afbdb9764b9cee4300
abcxys <abcxys92@gmail.com>
2024-06-25 22:48:40 -0400

    Add the initial UI and load excel function to show the first excel element.

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import sys
 import pandas as pd
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+class ExcelViewer(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-# Press the green button in the gutter to run the script.
+    def initUI(self):
+        self.layout = QVBoxLayout()
+
+        self.loadButton = QPushButton('Load Excel File', self)
+        self.loadButton.clicked.connect(self.loadExcel)
+        self.layout.addWidget(self.loadButton)
+
+        self.resultLabel = QLabel('Value will be displayed here', self)
+        self.layout.addWidget(self.resultLabel)
+
+        self.setLayout(self.layout)
+        self.setWindowTitle('Excel Viewer')
+        self.show()
+
+    def loadExcel(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Excel File", "", "Excel Files (*.xls *.xlsx);;All Files (*)", options=options)
+        if fileName:
+            df = pd.read_excel(fileName)
+            if not df.empty:
+                value = df.iloc[0, 0]
+                self.resultLabel.setText(f'First row, first column value: {value}')
+            else:
+                self.resultLabel.setText('The Excel file is empty')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = ExcelViewer()
+    sys.exit(app.exec_())
